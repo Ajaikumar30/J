6cc4d4090430346a29ce684875ac4f363a1b9675
Ajaikumar30 <kumarajai914@gmail.com>
2024-09-13 12:19:34 +0530

    ch2

---
+++ index.js
@@ -0,0 +1,26 @@
+// function create(...main){
+    
+//    for (i=main;i<main;i+=main){
+//               console.log(main)
+//    }
+// }
+// let sin=create(1,2,3,4)
+// console.log(sin)
+
+//  function submit(){
+
+//    let noofdice=document.getElementById("noofdice").value
+
+// let diceresult=document.getElementById("diceresult")
+// let diceimages=document.getElementById("diceimages")
+// let valu=[]
+// let img=[]
+//    for(i=0;i<noofdice;i++){
+//       let values=Math.floor(Math.random()*6)+1
+//       valu.push(values)
+//       img.push(`<img src="images/${values}.png">`)
+//    }
+   
+//     diceresult.textContent=`dice:${valu.join(",")}`
+//     diceimages.innerHTML=img.join("-")
+//  }
